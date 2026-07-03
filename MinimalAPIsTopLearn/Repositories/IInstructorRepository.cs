862013423269119effd027e42a9eb2aa71754e5c
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-07-03 22:58:05 +0330

    Added and Implemented IInstructorRepository.

---
+++ MinimalAPIsTopLearn/Repositories/IInstructorRepository.cs
@@ -0,0 +1,13 @@
+﻿using MinimalAPIsTopLearn.Entities;
+
+namespace MinimalAPIsTopLearn.Repositories;
+
+public interface IInstructorRepository
+{
+    Task<int> Create(InstructorInfo instructor);
+    Task Delete(int id);
+    Task<bool> Exists(int id);
+    Task<List<InstructorInfo>> GetAll();
+    Task<InstructorInfo?> GetById(int id);
+    Task Update(InstructorInfo instructor);
+}
---
+++ MinimalAPIsTopLearn/Repositories/InstructorRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIsTopLearn.Data;
+using MinimalAPIsTopLearn.Entities;
+
+namespace MinimalAPIsTopLearn.Repositories;
+
+public class InstructorRepository : IInstructorRepository
+{
+    private readonly AppDbContext _context;
+
+    public InstructorRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<InstructorInfo>> GetAll()
+    {
+        return await _context.Instructors.ToListAsync();
+    }
+    public async Task<InstructorInfo?> GetById(int id)
+    {
+        return await _context.Instructors.AsNoTracking().FirstOrDefaultAsync(a => a.Id == id);
+    }
+    public async Task<int> Create(InstructorInfo instructor)
+    {
+        await _context.AddAsync(instructor);
+        await _context.SaveChangesAsync();
+        return instructor.Id;
+    }
+    public async Task Update(InstructorInfo instructor)
+    {
+        _context.Update(instructor);
+        await _context.SaveChangesAsync();
+    }
+    public async Task Delete(int id)
+    {
+        await _context.Instructors.Where(x => x.Id == id).ExecuteDeleteAsync();
+    }
+    public async Task<bool> Exists(int id)
+    {
+        return await _context.Instructors.AnyAsync(x => x.Id == id);
+    }
+}
