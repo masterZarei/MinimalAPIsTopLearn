0a35e5ecb1aaa4c28c2242328f6cd4bade116bf3
Mohammad Mahdi Zarei <96488897+masterZarei@users.noreply.github.com>
2026-07-17 22:37:36 +0330

    Created an Endpoint for Getting Comments.

--- MinimalAPIsTopLearn/Endpoints/CommentsEndpoints.cs
+++ MinimalAPIsTopLearn/Endpoints/CommentsEndpoints.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.OutputCaching;
 using MinimalAPIsTopLearn.DTOs;
 using MinimalAPIsTopLearn.Entities;
 using MinimalAPIsTopLearn.Repositories;
@@ -9,24 +8,53 @@ namespace MinimalAPIsTopLearn.Endpoints;
 
 public static class CommentsEndpoints
 {
-    private readonly static string _cacheTag = "comments-get";
     public static RouteGroupBuilder MapComments(this RouteGroupBuilder builder)
     {
+        builder.MapGet("/", GetAll);
+        builder.MapGet("/{id:int}", GetById);
         builder.MapPost("/", Create);
         return builder;
     }
-    static async Task<Results<Created<CommentDTO>, NotFound>> Create(int courseId, 
-        CommentCreateDTO commentCreateDTO, ICourseRepository _courseRepo, ICommentRepository _commentRepo,
-        IOutputCacheStore _outputCacheStore,IMapper _mapper)
+    static async Task<Results<Ok<List<CommentDTO>>, NotFound>> GetAll(int courseId,
+        ICourseRepository _courseRepo, ICommentRepository _commentRepo,
+        IMapper _mapper)
     {
-        if(!await _courseRepo.Exists(courseId))
+        if (!await _courseRepo.Exists(courseId))
+        {
+            return TypedResults.NotFound();
+        }
+        var comments = await _commentRepo.GetAll(courseId);
+        var commentDto = _mapper.Map<List<CommentDTO>>(comments);
+        return TypedResults.Ok(commentDto);
+    }
+    static async Task<Results<Ok<CommentDTO>, NotFound>> GetById(int id, int courseId,
+        ICourseRepository _courseRepo, ICommentRepository _commentRepo,
+        IMapper _mapper)
+    {
+        if (!await _courseRepo.Exists(courseId))
+        {
+            return TypedResults.NotFound();
+        }
+        var comment = await _commentRepo.GetById(id);
+        if (comment is null)
+        {
+            return TypedResults.NotFound();
+        }
+        var commentDto = _mapper.Map<CommentDTO>(comment);
+        return TypedResults.Ok(commentDto);
+    }
+
+    static async Task<Results<Created<CommentDTO>, NotFound>> Create(int courseId,
+        CommentCreateDTO commentCreateDTO, ICourseRepository _courseRepo, ICommentRepository _commentRepo,
+        IMapper _mapper)
+    {
+        if (!await _courseRepo.Exists(courseId))
         {
             return TypedResults.NotFound();
         }
         var comment = _mapper.Map<CommentInfo>(commentCreateDTO);
         comment.CourseId = courseId;
         var id = await _commentRepo.Create(comment);
-        await _outputCacheStore.EvictByTagAsync(_cacheTag, default);
         var commentDto = _mapper.Map<CommentDTO>(comment);
         return TypedResults.Created($"/comments/{id}", commentDto);
     }
